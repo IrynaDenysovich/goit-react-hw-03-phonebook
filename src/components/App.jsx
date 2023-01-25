2bfd2dfe721407b0ddb143e9774894a9d16d08d5
IrynaDenysovich <106885843+IrynaDenysovich@users.noreply.github.com>
2023-01-25 15:29:18 +0200

    home work phonebook 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmit = ({ name, number }) => {
+    let filteredContact = this.state.contacts.filter(contact => {
+      return contact.name === name;
+    });
+    if (filteredContact.length === 0) {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, { id: nanoid(), name, number }],
+      }));
+    } else {
+      alert(`${name} is already in contacts.`);
+    }
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const visibleContacts = this.state.contacts.filter(contact =>
+      (contact.name + contact.number).toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <div
+        style={{
+          fontFamily: 'sans-serif',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+          flexDirection: 'column',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmit}></ContactForm>
+
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.changeFilter}></Filter>
+        <ContactList
+          contact={visibleContacts}
+          onDelete={this.deleteContact}
+        ></ContactList>
+      </div>
+    );
+  }
+}
